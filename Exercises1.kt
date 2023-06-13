61d5e3f57131636f1b7f69e413426309e8074d56
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-06-13 14:56:46 -0500

    Practice

---
+++ Exercises1.kt
@@ -0,0 +1,19 @@
+fun main() {
+//        val funds = 100
+//        val interest = 1.055
+//        val totalWithInterest = funds * interest * interest * interest * interest * interest
+//    println(totalWithInterest)
+
+    val fam = 3
+    val animals = 3
+    val producer = 1
+    if (fam >= 3){
+        if (producer >= 1){
+            if (animals <=5){
+                println(true)
+            } else {
+                println(false)
+            }
+        }
+    }
+}
